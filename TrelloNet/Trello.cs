5e7af4be237fd9a680e05e7d5e57844851bdbce0
ShTair <git@vsh.jp>
2018-10-04 13:56:12 +0900

    ボード一覧の取得を実装

--- TrelloNet/Trello.cs
+++ TrelloNet/Trello.cs
@@ -1,4 +1,7 @@
-﻿using System.Net.Http;
+﻿using Newtonsoft.Json;
+using ShComp.TrelloNet.Models;
+using System.Collections.Generic;
+using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace ShComp.TrelloNet
@@ -6,17 +9,23 @@ namespace ShComp.TrelloNet
     public class Trello
     {
         private readonly string _apiKey;
-        private readonly string _accessToken;
+        private readonly string _token;
 
-        public Trello(string apiKey, string accessToken)
+        public Trello(string apiKey, string token)
         {
             _apiKey = apiKey;
-            _accessToken = accessToken;
+            _token = token;
+        }
+
+        public async Task<List<Board>> GetBoardsAsync(string userId)
+        {
+            var json = await GetAsync($"https://trello.com/1/members/{userId}/boards?key={_apiKey}&token={_token}&fields=name");
+            return JsonConvert.DeserializeObject<List<Board>>(json);
         }
 
         #region Utils
 
-        private static async Task<string> GetAsync(string target)
+        private async Task<string> GetAsync(string target)
         {
             using (var hc = new HttpClient())
             {
@@ -24,7 +33,7 @@ namespace ShComp.TrelloNet
             }
         }
 
-        private static async Task PutAsync(string target, HttpContent content)
+        private async Task PutAsync(string target, HttpContent content)
         {
             using (var hc = new HttpClient())
             {
